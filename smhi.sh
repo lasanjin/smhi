3934d3e5d32b31d01779b7123761dd17d6fd3d4a
lasanjin <sanjinslavnic@gmail.com>
2019-04-22 21:03:56 +0200

    Formats code and ads README

--- smhi.sh
+++ smhi.sh
@@ -2,10 +2,10 @@
 
 smhi() {
     local ndays=1
-    if [ ! -z $1 ]; then
+    if ! isempty $1; then
         ndays=$1
 
-        if ! [[ "$1" =~ ^[0-9]*$ ]] || [ $1 -lt 0 ]; then
+        if ! isdigit $1 || isnegative $1; then
             echo -e "\nInvalid input\n"
             return 0
         fi
@@ -13,15 +13,15 @@ smhi() {
 
     smhi_url
     smhi_data $url
+    directory
     style
 
     read -r -a arr -d '' <<<"$data"
 
     local today=$(date +'%Y-%m-%d')
     local todate=$(date -d "$today+$ndays days" +'%s')
-
+    local dir=$dir
     declare local tmpdate
-    declare local tmptemp
 
     local length=${#arr[@]}
     for ((i = 0; i < $length; i += 5)); do
@@ -40,7 +40,7 @@ smhi() {
         local t=${arr[$((i + 3))]}
         local ws=${arr[$((i + 4))]}
 
-        if [ "$date" != "$tmpdate" ]; then
+        if ! equals $date $tmpdate; then
             local units='\t°C\tm/s\tmm\h\tdesc'
             local lang='sv_SE.utf-8'
             local day=$(LC_TIME=$lang date --date "$validTime" +'%A')
@@ -50,24 +50,20 @@ smhi() {
         fi
 
         local time=$(date --date "$validTime" '+%H:%M')
-        local description=$(sed "${Wsymb2}q;d" Wsymb2SV.txt)
+        local description=$(sed "${Wsymb2}q;d" $dir/Wsymb2SV.txt)
         local head=${dim}$time'\t'${default}${bold}
         local tail='\t'${blue}${ws}'\t'${pmin}${default}${dim}'\t'${description}${default}
 
-        if temp "30" "$t"; then
-            echo -e "${head}${red}${t}$tail"
-        elif temp "25" "$t"; then
-            echo -e "${head}${orange}${t}$tail"
-        elif temp "20" "$t"; then
+        if temp 30 $t; then
+            echo -e "${head}${red}${t}${tail}"
+        elif temp 25 $t; then
+            echo -e "${head}${orange}${t}${tail}"
+        elif temp 20 $t; then
             echo -e "${head}${yellow}${t}${tail}"
         else
             echo -e "${head}${blue}${t}${tail}"
         fi
     done
-}
-
-temp() {
-    [ 1 -eq $(echo ""$2" >= "$1"" | bc) ]
 }
 
 smhi_url() {
@@ -96,6 +92,30 @@ style() {
     orange='\e[38;5;202m'
     red='\e[31m'
     dim='\e[2m'
+}
+
+directory() {
+    dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+}
+
+temp() {
+    [ 1 -eq $(echo "$2 >= $1" | bc) ]
+}
+
+equals() {
+    [ "$1" == "$2" ]
+}
+
+isdigit() {
+    [[ "$1" =~ ^[0-9]*$ ]]
+}
+
+isnegative() {
+    [ $1 -lt 0 ]
+}
+
+isempty() {
+    [ -z $1 ]
 }
 
 smhi $1
